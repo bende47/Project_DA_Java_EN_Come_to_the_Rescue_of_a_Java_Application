e40a9e46f31149d9eee8952b08658a50dfed4ab6
bende47 <bendejustin@gmail.com>
2019-12-17 23:29:08 +0100

    Revision de code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,9 +1,6 @@
 package com.hemebiotech.analytics;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+
 import java.util.List;
 import java.util.Map;
 
@@ -13,20 +10,16 @@ public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
 		
-		 // reading the symptoms file
 		
         ReadSymptomDataFromFile rsdff = new ReadSymptomDataFromFile("./Project02Eclipse/symptoms.txt","./Project02Eclipse/result.out");
         List<String> symptoms = rsdff.GetSymptoms();
 
-        // count all occurrences of any symptoms listed in the file
         
         Map<String, Integer> groupedSymptoms = rsdff.groupSymptoms(symptoms);
 
-        // Sort the symptoms in alphabetical order
         
         groupedSymptoms = rsdff.sortSymptoms(groupedSymptoms);
 
-        // Generating a new text file named results.out that lists each symptom in alphabetical order.        
        
         rsdff.writeFile(groupedSymptoms);
 		
