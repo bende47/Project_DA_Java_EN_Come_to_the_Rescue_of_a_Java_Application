0ef96b53aacb70aba01321c453141bca3ee9d364
bende47 <bendejustin@gmail.com>
2019-12-28 10:45:29 +0100

    Appel des differentes methodes

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsMain.java
@@ -0,0 +1,23 @@
+package com.hemebiotech.analytics;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AnalyticsMain {
+
+	public static void main(String args[]) {
+
+		ISymptomReader rsdff = new ReadSymptomDataFromFile();
+		List<String> symptoms = rsdff.getSymptoms("./Project02Eclipse/symptoms.txt");
+
+		AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+		HashMap<String, Integer> groupedSymptoms = analyticsCounter.groupSymptoms(symptoms);
+
+		groupedSymptoms = analyticsCounter.sortSymptoms(groupedSymptoms);
+
+		rsdff.writeFile("./Project02Eclipse/result.out", groupedSymptoms);
+
+	}
+
+}
