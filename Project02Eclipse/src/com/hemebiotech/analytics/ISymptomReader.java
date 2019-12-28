95151f19e6144724d7a5f7ccd1169228aac04be7
bende47 <bendejustin@gmail.com>
2019-12-28 10:42:51 +0100

    Ajout de la methode writeFile et du parametre filepath a la mehode getSymptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,6 +1,8 @@
 package com.hemebiotech.analytics;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Anything that will read symptom data from a source
@@ -16,5 +18,6 @@ public interface ISymptomReader {
 	 * 
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-	List<String> GetSymptoms ();
+	 public List<String> getSymptoms (String filepath);
+	 public void writeFile(String fileOut, HashMap<String, Integer> groupedSymptoms);
 }
